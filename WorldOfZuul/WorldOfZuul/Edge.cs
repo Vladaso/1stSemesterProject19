1f62863ead73f238829d1dafaf94473d08ce231c
TZarins <tomasszarinsh@gmail.com>
2024-11-05 09:33:22 +0100

    Hi

---
+++ WorldOfZuul/WorldOfZuul/Edge.cs
@@ -0,0 +1,39 @@
+namespace WorldOfZuul
+{
+    public class Edge{
+        public int con1start {get; private set;}
+        public int con1end {get; private set;}
+        public string direction1 {get; private set;}
+
+        public int con2start {get; private set;}
+        public int con2end {get; private set;}
+        public string direction2 {get; private set;}
+    
+        public Edge(int con1, int con2, string dir){
+            con1start = con1;
+            con2end = con2;
+            direction1 = dir;
+
+            direction2 = changeDirection(dir);
+            con2start = con2;
+            con1end = con1;
+        }
+        private string changeDirection(string direction){
+            if (direction == "north"){
+                return "south";
+            }
+            else if (direction == "south"){
+                return "north";
+            }
+            else if (direction == "east"){
+                return "west";
+            }
+            else if (direction == "west"){
+                return "east";
+            }
+            else{
+                throw new Exception("Invalid direction");
+            }
+        }
+    }
+}
--- WorldOfZuul/WorldOfZuul/Game.cs
+++ WorldOfZuul/WorldOfZuul/Game.cs
@@ -3,142 +3,46 @@
 
     public class Game
     {
-        private Room? currentRoom;
-        private Room? previousRoom;
-
+        private Room[]? rooms;
+        private Edge[]? edges;
+        private RoomArt roomArt;
         public Game()
         {
+            roomArt = new RoomArt();
             CreateRooms();
+            CreateEdges();
         }
 
         private void CreateRooms()
         {
-            //Code bellow creates ROOMS, outside,theatre,pub... are the names for rooms
-            //First "" is Name of the room , second "" is LongDescription
-  
-            Room? outside = new("Outside", "You are standing outside the main entrance of the university. To the east is a large building, to the south is a computing lab, and to the west is the campus pub.");
-            Room? theatre = new("Theatre", "You find yourself inside a large lecture theatre. Rows of seats ascend up to the back, and there's a podium at the front. It's quite dark and quiet.");
-            Room? pub = new("Pub", "You've entered the campus pub. It's a cozy place, with a few students chatting over drinks. There's a bar near you and some pool tables at the far end.");
-            Room? lab = new("Lab", "You're in a computing lab. Desks with computers line the walls, and there's an office to the east. The hum of machines fills the room.");
-            Room? office = new("Office", "You've entered what seems to be an administration office. There's a large desk with a computer on it, and some bookshelves lining one wall.");
-
-
-            //Test Room
-
-            Room? test_room = new("TestRoom","This is test room"); //-> add RoomArt from list
-
-            //Code bellow creates possible exits for exact room
-
-            outside.SetExits(null, theatre, lab, pub); // North, East, South, West
-
-            theatre.SetExit("west", outside); //To get to theatre you have to type East in Outside room..
-
-            pub.SetExit("east", outside);
-
-            lab.SetExits(outside, office, null, null);
-
-            office.SetExit("west", lab);
-
-            currentRoom = outside;
+            rooms[0] = new Room(0, "Outside", "You are outside");
+            rooms[1] = new Room(1, "Inside", "You are inside");
         }
 
-        public void Play()
-        {
-            Parser parser = new();
-
-            PrintWelcome();
-
-            bool continuePlaying = true;
-            while (continuePlaying)
-            {
-                Console.WriteLine(currentRoom?.RoomName);
-                Console.Write("> ");
-
-                string? input = Console.ReadLine();
-
-                if (string.IsNullOrEmpty(input))
-                {
-                    Console.WriteLine("Please enter a command.");
-                    continue;
-                }
-
-                Command? command = parser.GetCommand(input);
-
-                if (command == null)
-                {
-                    Console.WriteLine("I don't know that command.");
-                    continue;
-                }
-
-                switch(command.Name)
-                {
-                    case "look":
-                        Console.WriteLine(currentRoom?.LongDescription);
-                        break;
-
-                    case "back":
-                        if (previousRoom == null)
-                            Console.WriteLine("You can't go back from here!");
-                        else
-                            currentRoom = previousRoom;
-                        break;
-
-                    case "north":
-                    case "south":
-                    case "east":
-                    case "west":
-                        Move(command.Name);
-                        break;
-
-                    case "quit":
-                        continuePlaying = false;
-                        break;
-
-                    case "help":
-                        PrintHelp();
-                        break;
-
-                    default:
-                        Console.WriteLine("I don't know what command.");
-                        break;
-                }
-            }
-
-            Console.WriteLine("Thank you for playing World of Zuul!");
+        private void CreateEdges(){
+            edges[0] = new Edge(0, 1, "north");
         }
 
-        private void Move(string direction)
-        {
-            if (currentRoom?.Exits.ContainsKey(direction) == true)
-            {
-                previousRoom = currentRoom;
-                currentRoom = currentRoom?.Exits[direction];
-            }
-            else
-            {
-                Console.WriteLine($"You can't go {direction}!");
-            }
-        }
+        // public void Play()
+        // {
+        //     Parser parser = new();
+
+        //     bool continuePlaying = true;
+        //     while (continuePlaying)
+        //     {
+        //         Console.Clear();
+        //         //Place for Screen Drawing
+
+        //         string input = parser.ReadAction(); // necessary side effect - prints "Invalid Action" if invalid and asks again
 
 
-        private static void PrintWelcome()
-        {
-            Console.WriteLine("Welcome to the World of Zuul!");
-            Console.WriteLine("World of Zuul is a new, incredibly boring adventure game.");
-            PrintHelp();
-            Console.WriteLine();
-        }
+        //         Action? action = parser.GetAction(input);
 
-        private static void PrintHelp()
-        {
-            Console.WriteLine("You are lost. You are alone. You wander");
-            Console.WriteLine("around the university.");
-            Console.WriteLine();
-            Console.WriteLine("Navigate by typing 'north', 'south', 'east', or 'west'.");
-            Console.WriteLine("Type 'look' for more details.");
-            Console.WriteLine("Type 'back' to go to the previous room.");
-            Console.WriteLine("Type 'help' to print this message again.");
-            Console.WriteLine("Type 'quit' to exit the game.");
-        }
+        //         action.Execute();
+
+        //     }
+
+        //     Console.WriteLine("Thank you for playing World of Zuul!");
+        // }
     }
 }
